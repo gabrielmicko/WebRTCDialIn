7e617d9f7f3799dc6735cad71838e7f59b664e67
Gabriel Mičko <micsko@gmail.com>
2016-05-01 13:46:48 +0200

    Start

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.use('/public', express.static('public'));
+
+
+app.get('*', function(req, resp) {
+	resp.sendFile(__dirname + '/index.html');
+});
+
+app.listen(8000, function () {
+  console.log('Example app listening on port 8000!');
+});
